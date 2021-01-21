9207066ddee8b4a6b9d973757819ad5897a2a53d
jpotel <jerome.potel@developpement-durable.gouv.fr>
2021-01-21 09:20:35 +0100

    Correction bug script python de fusion des pdf

--- fichiers/fusionPDF.py
+++ fichiers/fusionPDF.py
@@ -2,8 +2,8 @@ from PyPDF2 import PdfFileMerger, PdfFileReader
 import os, glob
 
 # à modifier en fonction du projet
-dossier = r'd:\dossier\contenant\fichiers\a\fusionner' 
-themeCarte = 'ZNS'
+dossier = r'D:\dossier\contenant\fichier\pdf' 
+themeCarte = 'Territoire'
 nbIdentifiantPerim = 5
 
 # création d'un dossier de sortie nommé output
@@ -17,12 +17,12 @@ for item in listFichier :
 listPerim = list(set(listPerim))
 
 # écriture des fichiers pdf fusionnés
-os.chdir(input)
+os.chdir(dossier)
 for item in listPerim :
     merger = PdfFileMerger()
-    files = [x for x in os.listdir(input) if x.startswith(item) and x.endswith('.pdf')]
+    files = [x for x in os.listdir(dossier) if x.startswith(item) and x.endswith('.pdf')]
     for fname in sorted(files):
-        with open(os.path.join(input, fname), 'rb') as f:
+        with open(os.path.join(dossier, fname), 'rb') as f:
             merger.append(PdfFileReader(f))
     print(item)
     merger.write(dossier+r'\output\\'+item+themeCarte+'.pdf')
